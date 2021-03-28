1207eccdcdc797a2b011b944fa873083607fb0d7
HelenArkh <alena.arkh@gmail.com>
2021-03-28 19:48:49 +0300

    Интеграционные тесты

--- UserTicketService/UserTicketService/TicketService.cs
+++ UserTicketService/UserTicketService/TicketService.cs
@@ -12,7 +12,7 @@ namespace UserTicketService
         {
             var ticket = FakeBaseData.FirstOrDefault(t => t.Id == ticketId);
             return (ticket is null) ?
-               throw new TicketNotFoundException() : ticket.Id;
+              throw new TicketNotFoundException() : ticket.Id;
         }
 
         public Ticket GetTicket(int ticketId)
@@ -22,17 +22,26 @@ namespace UserTicketService
               throw new TicketNotFoundException() : ticket;
         }
 
-        private IEnumerable<Ticket> FakeBaseData
+        public void SaveTicket(Ticket ticket)
         {
-            get
-            {
-                return new List<Ticket>
-         {
-            new Ticket(1, "Москва - Санкт-Петербург", 3500),
-            new Ticket(2, "Челябинск - Магадан", 3500),
-            new Ticket(3, "Норильск - Уфа", 3500)
-         };
-            }
+            FakeBaseData.Add(ticket);
         }
+
+        public void DeleteTicket(Ticket ticket)
+        {
+            FakeBaseData.Remove(ticket);
+        }
+
+        public IEnumerable<Ticket> GetAllTickets()
+        {
+            return FakeBaseData;
+        }
+
+        private List<Ticket> FakeBaseData = new List<Ticket>
+  {
+    new Ticket(1, "Москва - Санкт-Петербург", 3500),
+    new Ticket(2, "Челябинск - Магадан", 3500),
+    new Ticket(3, "Норильск - Уфа", 3500)
+  };
     }
 }
